81e88e9effe5822ac390ec05cfbadf3a6ff7601b
MatheoBenoit <matheo.benoit-paraschivoiu@polymtl.ca>
2021-03-18 19:31:48 -0700

    M: tout clean sauf le constructeur de FilmLivre

--- td4.cpp
+++ td4.cpp
@@ -5,7 +5,7 @@
 #pragma region "Includes"//{
 #define _CRT_SECURE_NO_WARNINGS // On permet d'utiliser les fonctions de copies de chaînes qui sont considérées non sécuritaires.
 
-#include "structures_solutionnaire_td3_5.hpp"      // Structures de données pour la collection de films en mémoire.
+#include "structures_td4.hpp"      // Structures de données pour la collection de films en mémoire.
 
 #include "bibliotheque_cours.hpp"
 #include "verification_allocation.hpp" // Nos fonctions pour le rapport de fuites de mémoire.
